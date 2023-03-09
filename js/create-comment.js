1837cedd60091627dbfc5fa4aaa1791cf6212342
Nastya <a.kochkina92@gmail.com>
2023-03-09 18:05:28 +0300

    модули

---
+++ js/create-comment.js
@@ -0,0 +1,44 @@
+import {getRandomInteger} from './util';
+import {getRandomArrayElement} from './util';
+
+const NAMES = [
+  'Маша', 'Паша', 'Глаша', 'Даша', 'Наташа', 'Степаша', 'Саша'
+];
+const AVATAR_COUNT = 6;
+const COMMENTS_LINES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палецgit  и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+//  Функция создает id для комментариев
+
+const createIDGenerator = () => {
+  let lastGeneratedID = 0;
+  return () => {
+    lastGeneratedID += 1;
+    return lastGeneratedID;
+  };
+};
+
+const createCommentID = createIDGenerator();
+
+// Функция создает содержание комментария
+
+const createMessage = () =>
+  Array.from({length: getRandomInteger(1, 2)}, () =>
+    getRandomArrayElement(COMMENTS_LINES)
+  ).join(' ');
+
+// Функция создает комментарий
+
+const createComment = () => ({
+  id: createCommentID(),
+  avatar: `img/avatar-${getRandomInteger(1,AVATAR_COUNT)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement (NAMES)
+});
+
+export {createComment};
---
+++ js/util.js
@@ -0,0 +1,36 @@
+// Функция генерирует случайное целое число
+
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Функция генерирует случайное целое число без повтора
+
+const createRandomIdFromRangeGenerator = (min, max) => {
+  const lastGeneratedID = [];
+
+  return function() {
+    let currentID = getRandomInteger(min, max);
+    if (lastGeneratedID.length >= (max - min + 1)) {
+      return null;
+    }
+    while (lastGeneratedID.includes(currentID)) {
+      currentID = getRandomInteger(min, max);
+    }
+    lastGeneratedID.push(currentID);
+    return currentID;
+  };
+};
+
+// Функция извлекает случайный элемент из массива
+
+const getRandomArrayElement = (array) =>
+  array[getRandomInteger(0, array.length - 1)];
+
+export {getRandomArrayElement};
+export {getRandomInteger};
+export {createRandomIdFromRangeGenerator};
+
